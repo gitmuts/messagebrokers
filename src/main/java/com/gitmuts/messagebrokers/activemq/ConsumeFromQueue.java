b20385411c5718d3f7b103f1ae5bec3322415f31
Gideon Kipkemoi [ICT] <gkipkemoi@CO-OPBANK.CO.KE>
2020-01-06 23:45:23 +0300

    rabbit mq changes

--- src/main/java/com/gitmuts/messagebrokers/activemq/ConsumeFromQueue.java
+++ src/main/java/com/gitmuts/messagebrokers/activemq/ConsumeFromQueue.java
@@ -2,7 +2,6 @@ package com.gitmuts.messagebrokers.activemq;
 
 import com.gitmuts.messagebrokers.model.Customer;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +9,9 @@ import org.springframework.stereotype.Service;
 public class ConsumeFromQueue {
 
 
-    @JmsListener( destination = "DLQ")
-    public void receiveFromQueue(Customer customer) {
-
-        log.info("Message {}", customer);
-    }
+//    @JmsListener( destination = "DLQ")
+//    public void receiveFromQueue(Customer customer) {
+//
+//        log.info("Message {}", customer);
+//    }
 }
