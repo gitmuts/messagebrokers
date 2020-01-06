750d627463d03a3d1e831309e2658ac0ea7c49a2
gitmuts <gideonm98@gmail.com>
2020-01-06 23:45:23 +0300

    rabbit mq changes

--- src/main/java/com/gitmuts/messagebrokers/activemq/MessageConvConfig.java
+++ src/main/java/com/gitmuts/messagebrokers/activemq/MessageConvConfig.java
@@ -3,7 +3,6 @@ package com.gitmuts.messagebrokers.activemq;
 import com.gitmuts.messagebrokers.model.Customer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,16 +10,16 @@ import java.util.Map;
 @Configuration
 public class MessageConvConfig {
 
-    @Bean
-    public MappingJackson2MessageConverter messageConverter() {
-        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
-
-        messageConverter.setTypeIdPropertyName("_typeId");
-        Map<String, Class<?>> typeIdMappings = new HashMap<String, Class<?>>();
-
-        typeIdMappings.put("customer", Customer.class);
-        messageConverter.setTypeIdMappings(typeIdMappings);
-
-        return messageConverter;
-    }
+//    @Bean
+//    public MappingJackson2MessageConverter messageConverter() {
+//        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+//
+//        messageConverter.setTypeIdPropertyName("_typeId");
+//        Map<String, Class<?>> typeIdMappings = new HashMap<String, Class<?>>();
+//
+//        typeIdMappings.put("customer", Customer.class);
+//        messageConverter.setTypeIdMappings(typeIdMappings);
+//
+//        return messageConverter;
+//    }
 }
--- src/main/java/com/gitmuts/messagebrokers/activemq/PublishToQueue.java
+++ src/main/java/com/gitmuts/messagebrokers/activemq/PublishToQueue.java
@@ -5,7 +5,6 @@ import com.gitmuts.messagebrokers.service.GenDataService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,29 +13,29 @@ import java.util.List;
 @Slf4j
 public class PublishToQueue {
 
-    @Autowired
-    JmsTemplate jmsTemplate;
-
-    @Autowired
-    GenDataService genDataService;
-
-    public void pushCustomerInfo() {
-        try{
-            List<Customer> customers = genDataService.getCustomers(20);
-
-            for(Customer customer: customers) {
-                log.debug("Pushing {}", customer);
-                jmsTemplate.convertAndSend("DLQ", customer);
-            }
-
-        }catch (Exception e){
-            log.error("Error ", e);
-        }
-    }
-
-    @Bean
-    public void test () {
-        pushCustomerInfo();
-    }
+//    @Autowired
+//    JmsTemplate jmsTemplate;
+//
+//    @Autowired
+//    GenDataService genDataService;
+//
+//    public void pushCustomerInfo() {
+//        try{
+//            List<Customer> customers = genDataService.getCustomers(20);
+//
+//            for(Customer customer: customers) {
+//                log.debug("Pushing {}", customer);
+//                jmsTemplate.convertAndSend("DLQ", customer);
+//            }
+//
+//        }catch (Exception e){
+//            log.error("Error ", e);
+//        }
+//    }
+//
+//    @Bean
+//    public void test () {
+//        pushCustomerInfo();
+//    }
 
 }
