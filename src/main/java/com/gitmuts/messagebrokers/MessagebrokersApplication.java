f1a2b3d7a45bb4ad8c8b489ad2cc4fca675eb846
Gideon Kipkemoi [ICT] <gkipkemoi@CO-OPBANK.CO.KE>
2020-01-06 15:43:36 +0300

    intial commit

---
+++ src/main/java/com/gitmuts/messagebrokers/MessagebrokersApplication.java
@@ -0,0 +1,13 @@
+package com.gitmuts.messagebrokers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagebrokersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagebrokersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitmuts/messagebrokers/MessagebrokersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitmuts.messagebrokers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagebrokersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
